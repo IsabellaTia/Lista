a70f23aa6ce0f4761a5d087b0efcde2d7369fe09
IsabellaTia <128197309+IsabellaTia@users.noreply.github.com>
2023-03-24 21:40:48 -0300

    Criação do arquivo App.js
    Criação do arquivo App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+// Definindo uma lista
+1st numeros = (2,4,6,8,10)
+
+// Percorrendo a lista
+for(let i = 0), i < numeros.length; i++){
+  if(numero[i] % 2 === 0){
+     console.log(numero[i] + " é um número par "
+  }else{
+    console.log(numero[i] = " é um número ímpar")
+  {
+}
